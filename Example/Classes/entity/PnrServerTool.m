42e999172e053e1942b1fc1163e7dd3430cb2c3e
popor <908891024@qq.com>
2019-03-14 23:52:39 +0800

    编辑重新提交js中

--- Example/Classes/entity/PnrServerTool.m
+++ Example/Classes/entity/PnrServerTool.m
@@ -249,19 +249,34 @@ static NSString * PnrWebCode1 = @"PnrWebCode1";
                     if (!feedback) {
                         feedback = @"NULL";
                     }
+                    PnrConfig * config      = [PnrConfig share];
+                    NSString * colorKey     = config.rootColorKeyHex;
+                    
                     NSMutableString * h5 = [NSMutableString new];
-                    [h5 setString:@"<html> <head><title>update</title></head> <body>"];
-                    [h5 appendString:@"<script>\
-                     window.onload=function (){\
+                    [h5 setString:@"<html> <head><title>update</title></head>"];
+                    
+                    // css
+                    [h5 appendString:@"\n<style type='text/css'>"];
+                    [h5 appendString:[self cssTextarea]];
+                    [h5 appendString:[self cssButton:colorKey]];
+                    [h5 appendString:@"\n</style>"];
+                    
+                    // body
+                    [h5 appendString:@"\n<body>"];
+                    [h5 appendString:[self jsonReadForm:@"feedback" key:PnrKeyConent name:@"返回数据" content:feedback]];
+                    
+                    [h5 appendString:@"\n<script>"];
+                    
+                    // js
+                    [h5 appendString:@"\n window.onload=function (){\
                      parent.parent.freshList();\
                      } "];
-                    [h5 appendString:[self jsonXmlJs]];
-                    [h5 appendString:@"</script>"];
+                    [h5 appendFormat:@"\n %@ \n %@ \n", [self textareaAutoHeightFuntion], [self textareaAuhoHeigtEventClass:PnrClassTaAutoH]];
                     
-                    [h5 appendString:[self jsonEditForm:@"feedback" key:PnrKeyConent name:@"返回数据" content:feedback cols:100 rows:10 textSize:16]];
-                    //[h5 appendFormat:@"<textarea wrap='on' cols='100' rows='10' style='font-size:%ipx;' > %@ </textarea>", 16, feedback];
+                    [h5 appendString:[self jsJsonStatic]];
+                    [h5 appendString:@"\n </script>"];
                     
-                    [h5 appendString:@"</body></html>"];
+                    [h5 appendString:@"\n</body></html>"];
                     complete([GCDWebServerDataResponse responseWithHTML:h5]);
                 };
                 GCDWebServerURLEncodedFormRequest * formRequest= (GCDWebServerURLEncodedFormRequest *)request;
@@ -322,12 +337,14 @@ static NSString * PnrWebCode1 = @"PnrWebCode1";
             h5 = [NSMutableString new];
             [h5 appendFormat:@"<html> <head><title>%@ 请求详情</title></head>", pnrTitle];
             
+            // css
             [h5 appendString:@"\n<style type='text/css'>"];
-            [h5 appendString:[self textareaCss]];
+            [h5 appendString:[self cssTextarea]];
+            [h5 appendString:[self cssButton:colorKey]];
             [h5 appendString:@"\n</style>"];
             
+            // body
             [h5 appendString:@"\n<body>"];
-            
             // 是否开启了重新提交
             if (self.resubmitBlock) {
                 [h5 appendFormat:@"<p> <a href='/%i/%@'> <button type='button' style=\"width:200px;\" > 重新请求 </button> </a> </p>", (int)index, PnrPathEdit];
@@ -338,42 +355,37 @@ static NSString * PnrWebCode1 = @"PnrWebCode1";
             [h5 appendFormat:@"<p><font color='%@'>%@</font><font color='%@'>%@</font></p>", colorKey, PnrRootMethod3, colorValue, pnrEntity.method];
             
             void (^ hrefBlock)(NSString*, id, NSString*) = ^(NSString * title, NSString * content, NSString * secondPath){
-                [h5 appendString:[self jsonReadForm:secondPath key:PnrKeyConent name:title content:content cols:100 rows:3 textSize:16 btColor:colorKey]];
+                [h5 appendString:[self jsonReadForm:secondPath key:PnrKeyConent name:title content:content]];
             };
-            
-            //            NSString * (^ webBlock)(NSString *, id) = ^(NSString * subtitle, NSString * content){
-            //                if (content) {
-            //                    return [NSString stringWithFormat:@"<html> <head><title>%@%@</title></head> <body><br/> <p>%@</p> </body></html>", pnrTitle, subtitle, content];
-            //                }else{
-            //                    return ErrorEmpty;
-            //                }
-            //            };
             
             hrefBlock(PnrRootHead4,      [self contentString:headStr],      PnrPathHead);
             hrefBlock(PnrRootParameter5, [self contentString:parameterStr], PnrPathParameter);
             hrefBlock(PnrRootResponse6,  [self contentString:responseStr],  PnrPathResponse);
             
-            //            self.h5Head     = webBlock(PnrRootHead4,      headStr);
-            //            self.h5Request  = webBlock(PnrRootParameter5, parameterStr);
-            //            self.h5Response = webBlock(PnrRootResponse6,  responseStr);
-            
-            [h5 appendFormat:@"\n<script> %@", [self jsonXmlJs]];
-            
-            [h5 appendFormat:@"%@ %@", [self textareaAutoHeightFuntion], [self textareaAuhoHeigtEventClass:PnrClassTaAutoH]];
-            
+            // js
+            [h5 appendFormat:@"\n<script> %@", [self jsJsonStatic]];
+            [h5 appendFormat:@"\n %@ %@", [self textareaAutoHeightFuntion], [self textareaAuhoHeigtEventClass:PnrClassTaAutoH]];
             [h5 appendString:@"</script>"];
             
             [h5 appendString:@"</body></html>"];
             self.h5Detail = h5;
         }
-        // 是否开启了重新提交
+        // MARK: 重新提交
         if (self.resubmitBlock) {
-            int textSize = 16;
             // 设置 h5Edit
             NSMutableString * h5;
             
             h5 = [NSMutableString new];
-            [h5 appendFormat:@"<html> <head><title>%@重新提交</title></head> <body>", pnrTitle];
+            [h5 appendFormat:@"<html> <head><title>%@重新提交</title></head>", pnrTitle];
+            
+            // css
+            [h5 appendString:@"\n<style type='text/css'>"];
+            [h5 appendString:[self cssTextarea]];
+            [h5 appendString:[self cssButton:colorKey]];
+            [h5 appendString:@"\n</style>"];
+            
+            // body
+            [h5 appendString:@"\n<body>"];
             
             [h5 appendFormat:@"<p> <a href='/%i/%@'> <button type='button' style=\"width:200px;\" > <==返回 </button> </a> </p>", (int)index, PnrPathDetail];
             
@@ -381,11 +393,13 @@ static NSString * PnrWebCode1 = @"PnrWebCode1";
             
             void (^ hrefBlock)(NSString*, NSString*, NSString*, int) = ^(NSString* title, NSString* key, NSString* value, int rows){
                 
-                [h5 appendFormat:@"<font color='%@'>%@</font><br> \
-                 <textarea id='%@' name='%@' wrap='on' cols='100' rows='%i' style='font-size:%ipx;' >%@</textarea> <br>", colorKey, title, key, key, rows, textSize, value];
-                
-                //[h5 appendFormat:@"<font color='%@'>%@</font><br> \
-                <textarea id='%@' name='%@' wrap='off' width='98%%' rows='2' style='font-size:%ipx;' >%@</textarea>  <br>", colorKey, title, key, key, textSize, value];
+                [h5 appendFormat:@"\n<br> <button type='button' \" onclick=\"jsonDynamic('%@', '%@')\" > %@ 查看详情 </button> ",
+                 PnrFormResubmit, key, title
+                 ];
+                //[h5 appendFormat:@"\n <br><font color='%@'>%@</font><br>",
+                //colorKey, title];
+                [h5 appendFormat:@"\n <textarea id='%@' name='%@' class='%@'>%@</textarea>",
+                 key, key, PnrClassTaAutoH, value];
             };
             
             hrefBlock(PnrRootPath1, @"url", [NSString stringWithFormat:@"%@/%@", pnrEntity.domain, pnrEntity.path], 1);
@@ -395,14 +409,20 @@ static NSString * PnrWebCode1 = @"PnrWebCode1";
             hrefBlock(@"额外参数", @"extra", extraStr, 4);
             
             //[h5 appendString:@"<br><input type=\"submit\" style=\"width:200px;\" value=\"  提交  \"> <br><br>"];
-            [h5 appendFormat:@"<p> <button type='button' style=\"width:200px;\" onclick=\"parent.resubmit()\" > 重新请求 </button>"];
-            [h5 appendFormat:@"&nbsp; <button type='button' style=\"width:200px;\" onclick=\"parent.freshList()\" > 刷新列表 </button> </p>"];
-            
+            [h5 appendFormat:@"<p> <button type='button' onclick=\"parent.resubmit()\" > 重新请求 </button>"];
+            [h5 appendFormat:@"&nbsp; <button type='button' onclick=\"parent.freshList()\" > 刷新列表 </button> </p>"];
             [h5 appendString:@"</form>"];
             
-            [h5 appendFormat:@"<iframe id='%@' name='%@' width ='900' height='400'></iframe>", PnrIframeFeedback, PnrIframeFeedback];
+            [h5 appendFormat:@"<iframe id='%@' name='%@' width ='800' height='400'></iframe>", PnrIframeFeedback, PnrIframeFeedback];
             
-            [h5 appendString:@"</body></html>"];
+            // js
+            [h5 appendFormat:@"\n<script> \n%@", [self jsJsonDynamic]];
+            [h5 appendFormat:@"\n %@ %@", [self textareaAutoHeightFuntion], [self textareaAuhoHeigtEventClass:PnrClassTaAutoH]];
+            
+            [h5 appendString:@"</script>"];
+            
+            
+            [h5 appendString:@"\n</body></html>"];
             self.h5Edit = h5;
         }
         
@@ -421,31 +441,36 @@ static NSString * PnrWebCode1 = @"PnrWebCode1";
     }
 }
 
-- (NSString *)jsonEditForm:(NSString *)form key:(NSString *)key name:(NSString *)keyName content:(NSString *)content cols:(int)cols rows:(int)rows textSize:(int)textSize {
-    
-    return [NSString stringWithFormat:@"<form id='%@' name='%@' method='POST' target='_blank' > <button type='button' style=\"width:80px;\" onclick=\"jsonXml('%@')\" > 查看详情 </button> <br> <textarea id='%@' name='%@' wrap='on' cols='%i' rows='%i' style='font-size:%ipx;' >%@</textarea> </form> ",
-            form, form, form, key, key,
-            cols, rows, textSize, content
-            ];
-    
-}
-
-- (NSString *)jsonReadForm:(NSString *)form key:(NSString *)key name:(NSString *)keyName content:(NSString *)content cols:(int)cols rows:(int)rows textSize:(int)textSize btColor:(NSString *)btColor {
-    
-    return [NSString stringWithFormat:@"\n<form id='%@' name='%@' method='POST' target='_blank' > \n <button type='button' style=\"color:%@; width:180px; font-size:16px; \" onclick=\"jsonXml('%@')\" > %@ 查看详情 </button> <br> \n <textarea id='%@' name='%@' class='%@'>%@</textarea> \n</form>",
-            form, form, btColor, form, keyName, key, key, PnrClassTaAutoH, content
+- (NSString *)jsonReadForm:(NSString *)form key:(NSString *)key name:(NSString *)keyName content:(NSString *)content {
+    return [NSString stringWithFormat:@"\n<form id='%@' name='%@' method='POST' target='_blank' > \n <button type='button' \" onclick=\"jsonStatic('%@')\" > %@ 查看详情 </button> <br> \n <textarea id='%@' name='%@' class='%@'>%@</textarea> \n</form>",
+            form, form, form, keyName, key, key, PnrClassTaAutoH, content
             ];
 }
 
-- (NSString *)jsonXmlJs {
+// MARK: 固定的查看json js代码
+- (NSString *)jsJsonStatic {
     return [NSString stringWithFormat: @"\n\
-            function jsonXml(formKey) {\n\
+            function jsonStatic(formKey) {\n\
             var form = document.getElementById(formKey);\n form.action='/%@';\n form.submit();\n\
             }\n\
             ", PnrPathJsonXml];
 }
 
-- (NSString *)textareaCss {
+// MARK: 动态的查看json js代码
+- (NSString *)jsJsonDynamic {
+    return [NSString stringWithFormat: @"\n\
+            function jsonDynamic(formKey, contentkey) {\n\
+            var valueFrom =  document.forms[formKey].elements[contentkey].value;\n\
+            var formNew =  new FormData();\n\
+            formNew.append(%@, valueFrom);\n\
+            formNew.method='post';\n\
+            formNew.action='/%@';\n\
+            formNew.submit();\n\
+            }\n\
+            ", PnrKeyConent, PnrPathJsonXml];
+}
+
+- (NSString *)cssTextarea {
     return @"\ntextarea {\n\
     border: 1px solid #eee;\n\
     padding: 5px;\n\
@@ -454,51 +479,21 @@ static NSString * PnrWebCode1 = @"PnrWebCode1";
     font-size:16px;\n\
     }";
 }
+
+- (NSString *)cssButton:(NSString *)btColor {
+    return [NSString stringWithFormat:@"\nbutton {\n\
+            color:%@; width:180px; font-size:16px;\
+            }\n", btColor];
+}
+
 // MARK: 高度自适应的textarea
 - (NSString *)textareaAutoHeightFuntion {
     // http://caibaojian.com/textarea-autoheight.html
     
     return @" function makeExpandingArea(el) { var setStyle = function (el) { el.style.height = 'auto'; el.style.height = el.scrollHeight + 'px'; }; var delayedResize = function (el) { window.setTimeout(function () { setStyle(el); }, 0); }; if (el.addEventListener) { el.addEventListener('input', function () { setStyle(el) }, false); setStyle(el); } else if (el.attachEvent) { el.attachEvent('onpropertychange', function () { setStyle(el); }); setStyle(el); } if (window.VBArray && window.addEventListener) { el.attachEvent('onkeydown', function () { var key = window.event.keyCode; if (key == 8 || key == 46) {delayedResize(el);} }); el.attachEvent('oncut', function () { delayedResize(el); }); } } ";
-    
-    // 包含\n
-    //    return @"\n\
-    //    function makeExpandingArea(el) {\n\
-    //    var setStyle = function (el) {\n\
-    //    el.style.height = 'auto';\n\
-    //    el.style.height = el.scrollHeight + 'px';\n\
-    //    }\n\
-    //    var delayedResize = function (el) {\n\
-    //    window.setTimeout(function () {\n\
-    //    setStyle(el)\n\
-    //    }, 0);\n\
-    //    }\n\
-    //    if (el.addEventListener) {\n\
-    //    el.addEventListener('input', function () {\n\
-    //    setStyle(el)\n\
-    //    }, false);\n\
-    //    setStyle(el)\n\
-    //    } else if (el.attachEvent) {\n\
-    //    el.attachEvent('onpropertychange', function () {\n\
-    //    setStyle(el)\n\
-    //    })\n\
-    //    setStyle(el)\n\
-    //    }\n\
-    //    if (window.VBArray && window.addEventListener) {\n\
-    //    el.attachEvent('onkeydown', function () {\n\
-    //    var key = window.event.keyCode;\n\
-    //    if (key == 8 || key == 46) delayedResize(el);\n\
-    //    \n\
-    //    });\n\
-    //    el.attachEvent('oncut', function () {\n\
-    //    delayedResize(el);\n\
-    //    });\n\
-    //    }\n\
-    //    }\n";
 }
 
 - (NSString *)textareaAuhoHeigtEventClass:(NSString *)className {
-    // 包含\n
-    //return [NSString stringWithFormat:@"var taArray = document.getElementsByClassName('%@'); \n for (var i = 0; i < taArray.length; i++) \n { \n makeExpandingArea(taArray[i]);\n };", className];
     return [NSString stringWithFormat:@"\n var taArray = document.getElementsByClassName('%@'); for (var i = 0; i < taArray.length; i++) { makeExpandingArea(taArray[i]); } \n", className];
 }
 
