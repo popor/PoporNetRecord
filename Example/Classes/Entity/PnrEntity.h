26c530c73f8e46490a8c7754e4c0357b6c63d7c4
wangkq <908891024@qq.com>
2019-03-19 09:42:39 +0800

    key值修改

--- Example/Classes/Entity/PnrEntity.h
+++ Example/Classes/Entity/PnrEntity.h
@@ -10,15 +10,15 @@
 
 typedef void(^PnrEntityBlock) (NSArray *titleArray, NSArray *jsonArray, NSMutableArray *cellAttArray);
 
-static NSString * PnrRootTitle0     = @"名称:";
-static NSString * PnrRootPath1      = @"接口:";
-static NSString * PnrRootUrl2       = @"链接:";
-static NSString * PnrRootTime3      = @"时间:";
-static NSString * PnrRootMethod4    = @"方法:";
-static NSString * PnrRootHead5      = @"head参数:";
-static NSString * PnrRootParameter6 = @"请求参数:";
-static NSString * PnrRootResponse7  = @"返回数据:";
-static NSString * PnrRootExtra8     = @"额外参数:";
+static NSString * PnrRootTitle0     = @"名称:  ";
+static NSString * PnrRootPath1      = @"接口:  ";
+static NSString * PnrRootUrl2       = @"链接:  ";
+static NSString * PnrRootTime3      = @"时间:  ";
+static NSString * PnrRootMethod4    = @"方法:  ";
+static NSString * PnrRootHead5      = @"head参数:  ";
+static NSString * PnrRootParameter6 = @"请求参数:  ";
+static NSString * PnrRootResponse7  = @"返回数据:  ";
+static NSString * PnrRootExtra8     = @"额外参数:  ";
 
 static int PnrListHeight            = 50;
 static NSString * PnrIframeList     = @"IframeList";
